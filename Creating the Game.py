82e4891fd1c48016840e33d935ed17e755f22478
Jett1909 <79547781+Jett1909@users.noreply.github.com>
2021-05-13 10:32:35 +1000

    Update Creating the Game.py

--- Creating the Game.py
+++ Creating the Game.py
@@ -1,4 +1,3 @@
-# buggy Rn
 import pygame, time
 pygame.init()
 #for the Background colour
@@ -6,10 +5,10 @@ gray = (119,118,110)
 #set the name of the Game
 pygame.display.set_caption('Car Racing Game')
 #Size of display screen
-display_width = 800
-display_height = 600
+WIDTH = 800
+HEIGHT = 600
 #creating the game display
-gamedisplay = pygame.display.set_mode((display_width, display_height))
+gamedisplay = pygame.display.set_mode((WIDTH, HEIGHT))
 #creates a clock to keep track of the time since the game was opened
 clock = pygame.time.Clock()
 carImg = pygame.image.load('cars.jpg')
@@ -18,32 +17,34 @@ def car(x, y):
     gamedisplay.blit(carImg, (x, y))
 
 def game_loop():
-    x = (display_width*0.35)
-    y = (display_height*0.6)
+    x = (WIDTH*0.35)
+    y = (HEIGHT*0.6)
+    x_change = 0
     #call global Variable
     global gray
-    #creating a loop to give the game a gray background
-    GameColor = True
-    if GameColor == True:
-        gamedisplay.fill(gray)
-        car(x, y)
-        pygame.display.update()
-        clock.tick(60)
+
     #Crating a loop to check if the X(quit) is pressed
-    gameLoop = False
-    while not gameLoop:
+    bumped = False
+    while not bumped:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                gameLoop = True
+                bumped = True
         if event.type==pygame.KEYDOWN:
             if event.key== pygame.K_LEFT:
                 x_change = -5
+                print("left Key pressed")
             if event.key == pygame.K_RIGHT:
                 x_change = 5
+                print("right key pressed")
         if event.type == pygame.KEYUP:
             if event.key== pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 x_change = 0  
         x += x_change
+        #creating a loop to give the game a gray background
+        gamedisplay.fill(gray)
+        car(x, y)
+        pygame.display.update()
+        clock.tick(60)
 game_loop()
 pygame.quit()
 quit()
